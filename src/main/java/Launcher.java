45ea6cb0b046b8099f604d0abac8375a4d64a6ec
XavierAMM <xaviermancero790@gmail.com>
2024-07-31 12:43:20 -0500

    added headless

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,7 +1,10 @@
+import javafx.application.Application;
 import server.ServerLauncher;
 
 public class Launcher {
     public static void main(String[] args) {
-        ServerLauncher.main(args);
+        //ServerLauncher.main(args);
+        System.setProperty("javafx.platform", "monocle");
+        Application.launch(ServerLauncher.class, args);
     }
 }
